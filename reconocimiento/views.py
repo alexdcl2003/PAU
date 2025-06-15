eb9679ff1cb2fe5169fa0a08aea6451c206092db
Eduardo <ealvaradoe2@unemi.edu.ec>
2025-06-15 00:46:17 -0500

    Menu_Logos_Urls

--- reconocimiento/views.py
+++ reconocimiento/views.py
@@ -1,18 +1,23 @@
 # reconocimiento/views.py
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Vista para el reconocimiento (ya está definida)
+@login_required
 def dashboard(request):
     return render(request, 'core/reconocimiento/dashboard.html')
 
+@login_required
 def monitor(request):
     return render(request, 'core/reconocimiento/monitor.html')
 
+@login_required
 def reportes(request):
     return render(request, 'core/reconocimiento/reportes.html')
 
+@login_required
 def usuarios(request):
     return render(request, 'core/reconocimiento/usuarios.html')
 
+@login_required
 def configuracion(request):
     return render(request, 'core/reconocimiento/configuracion.html')
